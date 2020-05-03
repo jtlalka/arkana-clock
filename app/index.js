fca4d137e3502966a0c9dd7feec5e19a70358afb
Jakub Tlalka <jakub@tlalka.net>
2020-05-03 20:18:34 +0200

    build: integrate project with Fitbit CLI

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import clock from "clock";
 import document from "document";
-import { preferences } from "user-settings";
+import {preferences} from "user-settings";
 import * as util from "../common/utils";
 
 // Update the clock every minute
@@ -11,15 +11,15 @@ const myLabel = document.getElementById("myLabel");
 
 // Update the <text> element every tick with the current time
 clock.ontick = (evt) => {
-  let today = evt.date;
-  let hours = today.getHours();
-  if (preferences.clockDisplay === "12h") {
-    // 12h format
-    hours = hours % 12 || 12;
-  } else {
-    // 24h format
-    hours = util.zeroPad(hours);
-  }
-  let mins = util.zeroPad(today.getMinutes());
-  myLabel.text = `${hours}:${mins}`;
+    let today = evt.date;
+    let hours = today.getHours();
+    if (preferences.clockDisplay === "12h") {
+        // 12h format
+        hours = hours % 12 || 12;
+    } else {
+        // 24h format
+        hours = util.zeroPad(hours);
+    }
+    let mins = util.zeroPad(today.getMinutes());
+    myLabel.text = `${hours}:${mins}`;
 }
