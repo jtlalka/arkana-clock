e9beb0d232ba380b27e3aa162fbc5831a6818174
Jakub Tlalka <jakub@tlalka.net>
2020-05-05 21:12:00 +0200

    feat: copy code from sdk-moment sample clock

--- app/index.js
+++ app/index.js
@@ -1,25 +1,89 @@
-import clock from "clock";
 import document from "document";
-import {preferences} from "user-settings";
-import * as util from "../common/utils";
 
-// Update the clock every minute
-clock.granularity = "minutes";
+import * as simpleActivity from "./simple/activity";
+import * as simpleClock from "./simple/clock";
+import * as simpleHRM from "./simple/hrm";
+import * as simpleSettings from "./simple/device-settings";
 
-// Get a handle on the <text> element
-const myLabel = document.getElementById("myLabel");
+let background = document.getElementById("background");
+let dividers = document.getElementsByClassName("divider");
+let txtTime = document.getElementById("txtTime");
+let txtDate = document.getElementById("txtDate");
+let txtHRM = document.getElementById("txtHRM");
+let iconHRM = document.getElementById("iconHRM");
+let imgHRM = iconHRM.getElementById("icon");
+let statsCycle = document.getElementById("stats-cycle");
+let statsCycleItems = statsCycle.getElementsByClassName("cycle-item");
 
-// Update the <text> element every tick with the current time
-clock.ontick = (evt) => {
-    let today = evt.date;
-    let hours = today.getHours();
-    if (preferences.clockDisplay === "12h") {
-        // 12h format
-        hours = hours % 12 || 12;
-    } else {
-        // 24h format
-        hours = util.zeroPad(hours);
-    }
-    let mins = util.zeroPad(today.getMinutes());
-    myLabel.text = `${hours}:${mins}`;
+/* --------- CLOCK ---------- */
+function clockCallback(data) {
+    txtTime.text = data.time;
+    txtDate.text = data.date;
 }
+
+simpleClock.initialize("minutes", "longDate", clockCallback);
+
+/* ------- ACTIVITY --------- */
+function activityCallback(data) {
+    statsCycleItems.forEach((item, index) => {
+        let img = item.firstChild;
+        let txt = img.nextSibling;
+        txt.text = data[Object.keys(data)[index]].pretty;
+        // Reposition the activity icon to the left of the variable length text
+        img.x = txt.getBBox().x - txt.parent.getBBox().x - img.width - 7;
+    });
+}
+
+simpleActivity.initialize("seconds", activityCallback);
+
+/* -------- HRM ------------- */
+function hrmCallback(data) {
+    txtHRM.text = `${data.bpm}`;
+    if (data.zone === "out-of-range") {
+        imgHRM.href = "images/heart_open.png";
+    } else {
+        imgHRM.href = "images/heart_solid.png";
+    }
+    if (data.bpm !== "--") {
+        iconHRM.animate("highlight");
+    }
+}
+
+simpleHRM.initialize(hrmCallback);
+
+/* -------- SETTINGS -------- */
+function settingsCallback(data) {
+    if (!data) {
+        return;
+    }
+    if (data.colorBackground) {
+        background.style.fill = data.colorBackground;
+    }
+    if (data.colorDividers) {
+        dividers.forEach(item => {
+            item.style.fill = data.colorDividers;
+        });
+    }
+    if (data.colorTime) {
+        txtTime.style.fill = data.colorTime;
+    }
+    if (data.colorDate) {
+        txtDate.style.fill = data.colorDate;
+    }
+    if (data.colorActivity) {
+        statsCycleItems.forEach((item, index) => {
+            let img = item.firstChild;
+            let txt = img.nextSibling;
+            img.style.fill = data.colorActivity;
+            txt.style.fill = data.colorActivity;
+        });
+    }
+    if (data.colorHRM) {
+        txtHRM.style.fill = data.colorHRM;
+    }
+    if (data.colorImgHRM) {
+        imgHRM.style.fill = data.colorImgHRM;
+    }
+}
+
+simpleSettings.initialize(settingsCallback);
